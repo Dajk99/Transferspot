17354c359704d5bfc856ee9e8cd0db7e864e53fb
Dawid <dknap009@gmail.com>
2022-10-20 14:28:06 +0200

    first commit - created connect.php file, js and scss folders with user files

---
+++ user/dashboard.php
@@ -0,0 +1,26 @@
+<?php
+
+@include "../components/connect.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Panel użytkownika</title>
+    <!-- link do Font Awesome -->
+    <script src="https://kit.fontawesome.com/e6c4644ded.js" crossorigin="anonymous"></script>
+    <!-- Połączenie ze stylami css -->
+    <link rel="stylesheet" href="../css/user_style.css">
+</head>
+<body>
+
+    <!-- Sekcja header -->
+    
+    <!-- Połączenie ze skryptem JS -->
+    <script src="../js/user_script.js"></script>
+</body>
+</html>
