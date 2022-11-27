8d727eca6f4c78a343f1f98cb88ba0b0a568c44b
Dawid <dknap009@gmail.com>
2022-11-27 11:05:00 +0100

    Fixed buttons in a header of user panel

--- components/user_header.php
+++ components/user_header.php
@@ -27,26 +27,24 @@
     <div class="underline"></div>
 
     <nav class="header__navbar navbar">
-        <button class="header__navbar-item btn navy-btn first-letter" onclick="location.href='update_profile.php'"><a class="view">Zaktualizuj profil</a></button>
-        <button class="header__navbar-item btn navy-btn first-letter" onclick="location.href='dashboard.php'">
+        <button class="header__navbar-item btn navy-btn btn-action first-letter" onclick="location.href='update_profile.php'"><a class="view">Zaktualizuj profil</a></button>
+        <button class="header__navbar-item btn navy-btn btn-action first-letter" onclick="location.href='dashboard.php'">
             <p>panel użytkownika</p>
         </button>
-        <button class="header__navbar-item btn navy-btn first-letter" onclick="location.href='add_ann.php'">
+        <button class="header__navbar-item btn navy-btn btn-action first-letter" onclick="location.href='add_ann.php'">
             <p>dodaj ogłoszenie</p>
         </button>
-        <button class="header__navbar-item btn navy-btn first-letter" onclick="location.href='view_ann.php'">
+        <button class="header__navbar-item btn navy-btn btn-action first-letter" onclick="location.href='view_ann.php'">
             <p>przeglądaj ogłoszenia</p>
         </button>
-        <button class="header__navbar-item btn navy-btn first-letter" onclick="location.href='user_register.php'">
+        <button class="header__navbar-item btn navy-btn btn-action first-letter" onclick="location.href='user_register.php'">
             <p>zarejestruj się</p>
         </button>
         <button class="header__navbar-item header__navbar-item--login btn first-letter" onclick="location.href='user_login.php'">
-            <i class="fa-solid fa-repeat"></i>
-            <a class="login">przełącz konto</a>
+            <a class="login"><i class="fa-solid fa-repeat"></i><p>przełącz konto</p></a>
         </button>
         <button class="header__navbar-item header__navbar-item--logout btn first-letter">
-            <i class="fa-solid fa-power-off"></i>
-            <a href="#" class="logout">wyloguj</a>
+            <a href="#" class="logout"><i class="fa-solid fa-power-off"></i><p>wyloguj</p></a>
         </button>
     </nav>
 
